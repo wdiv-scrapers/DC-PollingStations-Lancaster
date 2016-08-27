f8e703ca39875ea99ffa3a499546fc09d03c8a55
chris48s <c.mshaw@yahoo.co.uk>
2016-08-27 18:40:20 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Polling station and district data from Lancaster City Council
+
+This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
---
+++ scraper.py
@@ -0,0 +1,10 @@
+from arcgis_scraper import scrape
+
+
+stations_url = "https://localview.lancaster.gov.uk/ArcGIS/rest/services/elections/MapServer/2/query?geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=4326&outFields=*&f=pjson"
+districts_url = "https://localview.lancaster.gov.uk/ArcGIS/rest/services/elections/MapServer/5/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=4326&outFields=*&f=pjson"
+council_id = 'E07000121'
+
+
+scrape(stations_url, council_id, 'utf-8', 'stations')
+scrape(districts_url, council_id, 'utf-8', 'districts')
