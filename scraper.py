8462d2e7ada8e03214017fe145f6029ef92e9275
chris48s <c.mshaw@yahoo.co.uk>
2016-09-02 21:05:46 +0100

    ensure we always fetch features in the same order

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 from arcgis_scraper import scrape
 
 
-stations_url = "https://localview.lancaster.gov.uk/ArcGIS/rest/services/elections/MapServer/2/query?geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=4326&outFields=*&f=pjson"
-districts_url = "https://localview.lancaster.gov.uk/ArcGIS/rest/services/elections/MapServer/5/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=4326&outFields=*&f=pjson"
+stations_url = "https://localview.lancaster.gov.uk/ArcGIS/rest/services/elections/MapServer/2/query?geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&orderByFields=OBJECTID&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=4326&outFields=*&f=pjson"
+districts_url = "https://localview.lancaster.gov.uk/ArcGIS/rest/services/elections/MapServer/5/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&orderByFields=OBJECTID&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=4326&outFields=*&f=pjson"
 council_id = 'E07000121'
 
 
