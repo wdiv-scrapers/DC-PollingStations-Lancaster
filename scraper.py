8865ac2e57624a5cb1a74d67e16a386a8ae161b1
GeoWill <roper.william@gmail.com>
2021-03-10 10:28:15 +0000

    Update council id

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from dc_base_scrapers.arcgis_scraper import UnsortedArcGisScraper
 
 stations_url = "https://localview.lancaster.gov.uk/ArcGIS/rest/services/elections/MapServer/2/query?geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&orderByFields=OBJECTID&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=27700&outFields=*&f=pjson"
 districts_url = "https://localview.lancaster.gov.uk/ArcGIS/rest/services/elections/MapServer/5/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&orderByFields=OBJECTID&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=27700&outFields=*&f=pjson"
-council_id = 'E07000121'
+council_id = 'LAC'
 
 
 stations_scraper = UnsortedArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
